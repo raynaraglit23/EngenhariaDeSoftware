0acd59238c90d1e7094c36701e0a1219a658f208
Kelly Letícia <122893902+kellyleticia@users.noreply.github.com>
2025-07-13 01:44:29 -0300

    improving ui

--- src/components/EmptyState/EmptyState.jsx
+++ src/components/EmptyState/EmptyState.jsx
@@ -3,7 +3,7 @@ import styles from './EmptyState.module.css';
 
 export default function EmptyState({ icon, message }) {
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.container}>
       <div className={styles.icon}>{icon}</div>
       <p className={styles.text}>{message}</p>
     </div>
