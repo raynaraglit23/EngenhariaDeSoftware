35041f3fcffa1c647a99ed9feb3f1fce45f15466
raynaraglit23 <raynara.coelho45@aluno.ifce.edu.br>
2025-07-15 15:22:02 -0300

    coordenadas atualizadas

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -6,27 +6,40 @@ export default function WeatherCard({ data }) {
 
   const {
     name,
+    country,
+    latitude,
+    longitude,
     main: { temp, feels_like, humidity },
     weather: [first],
   } = data;
+
   const { description, icon } = first;
 
   return (
     <div className={styles.card}>
-      <h2 className={styles.city}>{name}</h2>
+      <h2 className={styles.city}>
+        {name}, {country}
+      </h2>
+
       <img
         className={styles.icon}
         src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
         alt={description}
       />
+
       <p className={styles.temp}>
         {Math.round(temp)}°C{' '}
         <span className={styles.feels}>
           (sensação: {Math.round(feels_like)}°C)
         </span>
       </p>
+
       <p className={styles.humidity}>Umidade: {humidity}%</p>
       <p className={styles.desc}>{description}</p>
+
+      <p className={styles.coords}>
+        <strong>Coordenadas:</strong> {latitude}, {longitude}
+      </p>
     </div>
   );
 }
